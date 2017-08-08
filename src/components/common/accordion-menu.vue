6d47da60610ba1af1b383be95337d94cdd8318d2
Cristian Maghiar <cristi@avangard-media.ro>
2017-08-08 20:04:43 +0300

    #16 added accordion menu

---
+++ src/components/common/accordion-menu.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="panel-group" id="accordion" role="tablist" :aria-multiselectable="oneAtATime">
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="heading-1">
+        <h4 class="panel-title">
+          <v-touch tag="a" @tap="categoriesOpen = !categoriesOpen" role="button" data-toggle="collapse" data-parent="#accordion" :aria-expanded="categoriesOpen" aria-controls="collapse-1">
+            Categories
+          </v-touch>
+        </h4>
+      </div>
+      <div id="collapse-1" class="panel-collapse collapse" :class="{'in': categoriesOpen}" :style="panelStyle(categoriesOpen)" role="tabpanel" aria-labelledby="heading-1">
+        <div class="panel-body">
+          <ul>
+            <li v-for="category in accData" :key="category.id">
+              <router-link :to="journalByCatIdRoute(category.id)">{{category.name}}</router-link> ({{category.count}})
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="heading-2">
+        <h4 class="panel-title">
+          <v-touch tag="a" @tap="archivesOpen = !archivesOpen" role="button" data-toggle="collapse" data-parent="#accordion" :aria-expanded="archivesOpen" aria-controls="collapse-2">
+            Archives
+          </v-touch>
+        </h4>
+      </div>
+      <div id="collapse-2" class="panel-collapse collapse" :class="{'in': archivesOpen}" :style="panelStyle(archivesOpen)" role="tabpanel" aria-labelledby="heading-2">
+        <div class="panel-body">
+          <ul>
+            <li>2015</li>
+            <li>2016</li>
+            <li>2017</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  const byCat = '/journal/category/'
+  export default {
+    name: 'accordion-menu',
+    props: {
+      oneAtATime: {
+        type: Boolean,
+        required: true
+      },
+      accData: {
+        type: Array,
+        required: true
+      }
+    },
+    data () {
+      return {
+        categoriesOpen: true,
+        archivesOpen: false
+      }
+    },
+    methods: {
+      journalByCatIdRoute (id) {
+        return byCat + id
+      },
+      panelStyle (val) {
+        return {
+          height: val ? '' : '0'
+        }
+      }
+    }
+  }
+</script>
--- src/components/journal/journal.vue
+++ src/components/journal/journal.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="journal" class="row page">
     <div class="col-xs-2">
-      <ul>
-        <li v-for="category in categoriesData" :key="category.id">
-          <router-link :to="journalByCatIdRoute(category.id)">{{category.name}} ({{category.count}})</router-link>
-        </li>
-      </ul>
+      <accordion-menu :one-at-a-time="true" :acc-data="categoriesData"/>
     </div>
     <div class="col-xs-10">
       <div class="row">
@@ -13,7 +9,7 @@
           <span v-html="journalData.content.rendered"/>
         </div>
         <div class="col-xs-6" v-for="post in posts" :key="post.id">
-          <span v-html="post.images[0]"/><br>
+          <!--<span v-html="post.images[0]"/><br>-->
           <span v-html="post.title"/><br>
           <span v-html="post.excerpt"/> <br>
           <span v-html="post.date"/> <br>
@@ -26,11 +22,11 @@
 <script>
   import {mapGetters} from 'vuex'
   import Config from '../../api/app.config'
-
-  const byCat = '/journal/category/'
+  import accordionMenu from '../common/accordion-menu.vue'
 
   export default {
     name: 'journal-page',
+    components: {accordionMenu},
     computed: {
       ...mapGetters(['pages', 'posts', 'categories']),
       journalData () {
@@ -43,11 +39,6 @@
       },
       postsData () {
         return this.posts
-      }
-    },
-    methods: {
-      journalByCatIdRoute (id) {
-        return byCat + id
       }
     }
   }
