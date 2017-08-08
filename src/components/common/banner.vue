7d494d9591b9d2fbb7fb5b43bfec53ae995b69f3
Cristian Maghiar <cristi@avangard-media.ro>
2017-08-08 16:21:36 +0300

    #4 make image-overlayed reusable

--- src/components/common/banner.vue
+++ src/components/common/banner.vue
@@ -2,19 +2,13 @@
   <div class="home-banner-inner">
     <template v-if="banner.routeTo">
       <router-link :to="banner.routeTo">
-        <span class="banner-image-wrapper">
-          <img :src="banner.better_featured_image.source_url" class="banner-image"/>
-          <span class="banner-image-overlay"/>
-        </span>
+        <image-overlayed :url="banner.better_featured_image.source_url"/>
       </router-link>
       <router-link :to="banner.routeTo" class="banner-title">{{banner.title.rendered}}</router-link>
     </template>
     <template v-else>
       <a href="http://eepurl.com/lobiL" target="_new">
-        <span class="banner-image-wrapper">
-          <img :src="banner.better_featured_image.source_url" class="banner-image"/>
-          <span class="banner-image-overlay"/>
-        </span>
+        <image-overlayed :url="banner.better_featured_image.source_url"/>
       </a>
       <a href="http://eepurl.com/lobiL" target="_new" class="banner-title">{{banner.title.rendered}}</a>
     </template>
@@ -23,8 +17,11 @@
 </template>
 
 <script>
+  import imageOverlayed from './image-overlayed.vue'
+
   export default {
     name: 'banner',
+    components: {imageOverlayed},
     props: {
       banner: {
         type: Object,
---
+++ src/components/common/image-overlayed.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="image-overlayed-wrapper">
+    <img :src="url" class="image-overlayed"/>
+    <span class="image-overlay"/>
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'image-overlayed',
+    props: {
+      url: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
