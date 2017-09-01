82ac2f3a23a7256e84bf48fcf533d5006c74eecb
Cristian Maghiar <cristi@avangard-media.ro>
2017-09-01 18:04:08 +0300

    not sure

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,7 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var PrerenderSpaPlugin = require('prerender-spa-plugin')
+// var PrerenderSpaPlugin = require('prerender-spa-plugin')
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : config.build.env
@@ -93,20 +93,20 @@ var webpackConfig = merge(baseWebpackConfig, {
         inject: 'head',
         ignore: ['.*']
       }
-    ]),
+    ])
     // prerender the crawlable routes here
-    new PrerenderSpaPlugin(
-      path.join(__dirname, '../dist'),
-      routes,
-      {
-        // Instead of loudly failing on JS errors (the default), ignore them.
-        ignoreJSErrors: true,
-        // http://phantomjs.org/api/webpage/property/settings.html
-        phantomPageSettings: {
-          loadImages: true
-        }
-      }
-    )
+    // new PrerenderSpaPlugin(
+    //   path.join(__dirname, '../dist'),
+    //   routes,
+    //   {
+    //     // Instead of loudly failing on JS errors (the default), ignore them.
+    //     ignoreJSErrors: true,
+    //     // http://phantomjs.org/api/webpage/property/settings.html
+    //     phantomPageSettings: {
+    //       loadImages: true
+    //     }
+    //   }
+    // )
   ]
 })
 
