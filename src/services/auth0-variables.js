94f00807146accd6828c59389cc306af6f1a1c38
Cristian Maghiar <cristi@avangard-media.ro>
2017-09-05 16:38:12 +0300

    start as you mean to go on

--- src/services/auth0-variables.js
+++
@@ -1,7 +0,0 @@
-// auth0-variables.js.example
-export const AUTH_CONFIG = {
-  clientId: '{CLIENT_ID}',
-  domain: '{DOMAIN}',
-  callbackUrl: 'http://localhost:8080/callback',
-  apiUrl: '{API_IDENTIFIER}'
-}
