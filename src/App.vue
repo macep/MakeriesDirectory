3f941cb9e246c0fc6f9c9861d1e160838a26df8e
Cristian Maghiar <cristi@avangard-media.ro>
2018-09-22 07:49:14 +0300

    ping new api

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@
   import jgmFooter from './components/layout/jgm-footer.vue'
   import Config from './api/app.config'
   import AuthService from './api/auth.service'
+//  import axios from 'axios'
 
   const auth = new AuthService()
   const {login, socialLogin, logout, authenticated, authNotifier} = auth
@@ -75,6 +76,15 @@
         }, 500)
       })
       makeBodyClass(this.$route)
+//      axios({
+//        method: 'get',
+//        url: 'http://makeries.datatechrom.com/doc/v2/#/maker',
+//        headers: {
+//          'content-type': 'application/json',
+//          'accept': 'application/json',
+//          'access-control-allow-origin': '*'
+//        }
+//      }).then((res, err) => console.log(res, err))
     },
     computed: {
       ...mapGetters([
