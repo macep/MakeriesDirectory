64b0d52b5d61c240340d0b8da88336e282f2846a
Cristian Maghiar <cristi@avangard-media.ro>
2018-12-08 01:36:59 +0200

    new production target

--- src/api/app.config.js
+++ src/api/app.config.js
@@ -1,4 +1,4 @@
-const wpDomain = process.env.NODE_ENV === 'development' ? 'http://justgotmade.com:8888/' : `${window.location.origin}/website/`
+const wpDomain = process.env.NODE_ENV === 'development' ? 'http://justgotmade.com:8888/' : 'http://ui.macep23.com/website/'
 
 export default {
   apiUrlBase: '/',
